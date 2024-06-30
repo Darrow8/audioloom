0e00900bbb8f7427d796278deeef5c0655e746f9
Darrow Hartman <darrowrhartman@gmail.com>
2024-06-30 15:45:57 -0700

    fixed openai API issues and setup dockerfile

--- backend/genai.js
+++ backend/genai.js
@@ -1,22 +1,68 @@
-import OpenAI from "openai";
+import OpenAI, { toFile } from "openai";
+import fs from "fs";
+import path from "path";
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
 
-const openai = new OpenAI();
+dotenv.config();
+
+
+const openai = new OpenAI({
+    organization: "org-nd9Kz3AFMD7Wo3q05FTQMFAw",
+    project: "proj_6RLSgyTB2eo5NjAfghsFU8df",
+    apiKey: process.env['OPENAI_API_KEY'],
+});
+
+// const filePath = path.join(__dirname, '../test_result/article.txt');
+
 
 async function scriptwriter() {
+  const fileContents = fs.readFileSync("../test_result/article.txt").toString()
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    messages: [{ role: "system", 
+    content: 
+    `
+    You will receive a document that contains an informative article. 
+    The document starts at "DOCUMENT_START" and ends at "DOCUMENT_END".
+    Your job is to convert this article into a script for a podcast that contains all the important information about the article. 
+    This podcast script should contain enough content for a 30 minute episode. 
+    You may include external information for context if necessary.
+    DOCUMENT_START
+    ${fileContents}
+    DOCUMENT_END
+    ` }],
     model: "gpt-4-turbo",
   });
 
-  console.log(completion.choices[0]);
+  console.log(completion.choices[0].message.content);
 }
 
+scriptwriter()
 
-async function trimmer() {
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: "You will receive a .txt file that may be poorly formatted. Your job is to trim the contents of this article to just include the useful information about this article." }],
-      model: "gpt-4-turbo",
-    });
+// async function trimmer() {
+//     const fileContents = fs.readFileSync("../test_result/article.txt").toString()
+//     const completion = await openai.chat.completions.create({
+//       messages: [
+//         {   role: "system", 
+//             content: `
+//             You will receive a document that may be poorly formatted. 
+//             Your job is to trim the document just include the information of the article. Delete unecessary newlines.
+//             `
+//         },
+//         {
+//             role: 'user',
+//             content: `Please process this:
+//             DOCUMENT_START
+//             ${fileContents}
+//             DOCUMENT_END
+//             `,
+
+//           } 
+//     ],
+//       model: "gpt-4-turbo",
+//     });
+
   
-    console.log(completion.choices[0]);
-  }
+//     console.log(completion.choices[0]);
+//   }
+  // trimmer()
