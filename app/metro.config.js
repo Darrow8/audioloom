27bf6afb9018391bf35cc08c0569784227e6576a
Darrow Hartman <darrowrhartman@gmail.com>
2024-12-31 07:31:39 -0800

    fixed swipe up bug and bug with scroll overlapping with upload button

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,23 +1,16 @@
-// // Learn more https://docs.expo.io/guides/customizing-metro
-// const { getDefaultConfig } = require('expo/metro-config');
-
-// /** @type {import('expo/metro-config').MetroConfig} */
-// const config = getDefaultConfig(__dirname);
-
-// module.exports = config;
-
-
-const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
+const {
+  getSentryExpoConfig
+} = require("@sentry/react-native/metro");
 
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '..');
 
-const config = getDefaultConfig(projectRoot);
+const config = getSentryExpoConfig(projectRoot);
 
 // Add shared directory to watchFolders
 config.watchFolders = [
-  ...config.watchFolders || [],
+  ...(config.watchFolders || []),
   path.resolve(workspaceRoot, 'shared')
 ];
 
